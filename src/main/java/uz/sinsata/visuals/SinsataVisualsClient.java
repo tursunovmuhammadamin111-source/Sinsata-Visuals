c5e042edcb880d99d0f7be6bf870d30e8dada577
tursunovmuhammadamin111-source <tursunovmuhammadamin111@gmail.com>
2026-06-22 07:32:15 +0300

    Update SinsataVisualsClient.java

--- src/main/java/uz/sinsata/visuals/SinsataVisualsClient.java
+++ src/main/java/uz/sinsata/visuals/SinsataVisualsClient.java
@@ -5,7 +5,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.fabricmc.fabric.api.event.client.ClientTickEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import org.lwjgl.glfw.GLFW;
 import uz.sinsata.visuals.config.VisualsConfig;
 import uz.sinsata.visuals.gui.SinsataVisualsScreen;
